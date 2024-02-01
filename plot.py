cc4e869790782860eb2c6d2c716b9d1b7b158256
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-02-01 15:17:14 +0100

    Add simple3 model and add dpi to plot script

--- plot.py
+++ plot.py
@@ -7,17 +7,12 @@ from tensorflow.keras.layers import Conv3D, MaxPooling3D, Conv2D, MaxPooling2D, 
 model_name = input("Enter model name: ")
 model = Sequential()
 # INSERT MODEL HERE, REMEMBER input_shape= ... ...(128, 128, 3) ... ... 
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(224,224,3)))
+model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=(224,224,3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(128, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 # INSERT MODEL ABOVE, REMEMBER input_shape= ... ...(128, 128, 3) ... ...
-plot_model(model, to_file=f'./documentation/images/{model_name}.png', show_shapes=True, show_layer_names=True)
+plot_model(model, to_file=f'./documentation/images/{model_name}.png', show_shapes=True, show_layer_names=True,dpi=300)
 
